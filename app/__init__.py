49717f1d7fa1e261986968071d4431d37e2846b5
Dennis Wachira <wachiradenis336@gmail.com>
2025-06-17 20:10:01 +0300

    git commit -m "feat: implement RESTful routes for heroes, powers, and hero powers"

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,10 @@ from flask_migrate import Migrate
 from .extensions import db
 from .models import Hero, Power, HeroPower
 from .routes import register_routes
+from flask_restful import Api
+from app.resources.hero_resource import HeroListResource, HeroDetailResource
+from app.resources.power_resource import PowerListResource, PowerDetailResource
+from app.resources.hero_power_resource import HeroPowerListResource
 
 def create_app():
     app = Flask(__name__)
@@ -13,6 +17,13 @@ def create_app():
     db.init_app(app)
     register_routes(app)
     Migrate(app, db)
+
+    api = Api(app)
+    api.add_resource(HeroListResource, "/heroes")
+    api.add_resource(HeroDetailResource, "/heroes/<int:id>")
+    api.add_resource(PowerListResource, "/powers")
+    api.add_resource(PowerDetailResource, "/powers/<int:id>")
+    api.add_resource(HeroPowerListResource, "/hero_powers")
 
     return app
     
